4e5b0db75cffab7be781a29feb876fe7dddd1f6b
karmina-jc <karmina.jarac@gmail.com>
2020-05-02 22:41:42 -0400

    Updates

---
+++ precache-manifest.f44f74ed9abeb80a1470885541512a3b.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d3d89e3ec9faab4347f8218773cc00",
+    "url": "/karmina-jc/index.html"
+  },
+  {
+    "revision": "8dae417b5fc246e45e0c",
+    "url": "/karmina-jc/static/css/main.c6dd1892.chunk.css"
+  },
+  {
+    "revision": "21cfccf80526d5342373",
+    "url": "/karmina-jc/static/js/2.2d9fa420.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/karmina-jc/static/js/2.2d9fa420.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dae417b5fc246e45e0c",
+    "url": "/karmina-jc/static/js/main.8e735264.chunk.js"
+  },
+  {
+    "revision": "f5d0a5842f795e42c7b2",
+    "url": "/karmina-jc/static/js/runtime-main.2ecbed49.js"
+  },
+  {
+    "revision": "d746967fc52bf358908561c6792f9d1a",
+    "url": "/karmina-jc/static/media/avatar.d746967f.png"
+  },
+  {
+    "revision": "40db027dad88401bc5e74cc4a36c4034",
+    "url": "/karmina-jc/static/media/github-green.40db027d.png"
+  },
+  {
+    "revision": "b885ee7fc5336357c4a097f997ae1b45",
+    "url": "/karmina-jc/static/media/github-white.b885ee7f.png"
+  },
+  {
+    "revision": "04b87d2e822108c72051db38065b04d3",
+    "url": "/karmina-jc/static/media/hexagono-footer-green.04b87d2e.png"
+  },
+  {
+    "revision": "2ecdcef42b4f4585371af73b2d15dc50",
+    "url": "/karmina-jc/static/media/hexagono-footer.2ecdcef4.png"
+  },
+  {
+    "revision": "994bf1fea677a87c5e61d7be0f74f0c0",
+    "url": "/karmina-jc/static/media/hexagono-full-purple.994bf1fe.png"
+  },
+  {
+    "revision": "a274756efcc4569310dc9742875cc089",
+    "url": "/karmina-jc/static/media/linkedin-green.a274756e.png"
+  },
+  {
+    "revision": "53baeaa76a0f5f1dd70734495ee2f61b",
+    "url": "/karmina-jc/static/media/logo-MDlinks.53baeaa7.png"
+  },
+  {
+    "revision": "a6099cf78b71b7f3fc565f8f1aa2e5fb",
+    "url": "/karmina-jc/static/media/logo-boceto.a6099cf7.png"
+  },
+  {
+    "revision": "80a8e0d39f55c5dd9291763fe01cb0dd",
+    "url": "/karmina-jc/static/media/logo-bootstrap.80a8e0d3.png"
+  },
+  {
+    "revision": "a60fcc89be37a377a7e1996231a9371c",
+    "url": "/karmina-jc/static/media/logo-cool-girls.a60fcc89.png"
+  },
+  {
+    "revision": "ea8e6bcd6478bd58ed82dc871e6b2c71",
+    "url": "/karmina-jc/static/media/logo-css.ea8e6bcd.png"
+  },
+  {
+    "revision": "471ad3e7b683e7196b638cd73e38697b",
+    "url": "/karmina-jc/static/media/logo-figma.471ad3e7.png"
+  },
+  {
+    "revision": "9ca48e639c6bac5636cda8022bef41f4",
+    "url": "/karmina-jc/static/media/logo-fire.9ca48e63.png"
+  },
+  {
+    "revision": "e2033cc6464da46f8cfbb1c8ba9df0da",
+    "url": "/karmina-jc/static/media/logo-html.e2033cc6.png"
+  },
+  {
+    "revision": "8187196ee173165267a58ed2b0653c41",
+    "url": "/karmina-jc/static/media/logo-javascript.8187196e.png"
+  },
+  {
+    "revision": "0a04bddc0c4715ff04e22e2983dc9963",
+    "url": "/karmina-jc/static/media/logo-mytops.0a04bddc.png"
+  },
+  {
+    "revision": "820ff2208814fb16d71d2f591b2db771",
+    "url": "/karmina-jc/static/media/logo-node.820ff220.png"
+  },
+  {
+    "revision": "bfe9e997fe05e2af3a26fb3d75f4c017",
+    "url": "/karmina-jc/static/media/logo-react.bfe9e997.png"
+  },
+  {
+    "revision": "736e5b8bb6fb6ccda172e20e41c66cc5",
+    "url": "/karmina-jc/static/media/portfolio-green.736e5b8b.png"
+  },
+  {
+    "revision": "440ca47a8ea940ce0f0eca558e8eeb6c",
+    "url": "/karmina-jc/static/media/rocket-white.440ca47a.png"
+  }
+]);
